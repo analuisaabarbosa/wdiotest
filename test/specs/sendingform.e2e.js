c2e84ae4bd3f86fe432ce20290881de99de426a2
ana <analuisaaugustob@gmail.com>
2023-07-10 19:59:22 -0300

    Otimizando código

--- test/specs/sendingform.e2e.js
+++ test/specs/sendingform.e2e.js
@@ -4,8 +4,9 @@ describe('Testing the function FORMS in Webdriver.IO', () => {
         await $('~text-input').setValue('Texto texto')
         await $('~input-text-result').getText('Texto texto') 
         await $('~switch').click()
-        await $('android.widget.EditText').click()
-        await $('android.view.ViewGroup').click()
-        await $('android.widget.Button').click()
+        await $('//android.view.ViewGroup[@content-desc="Dropdown"]/android.view.ViewGroup/android.widget.EditText').click()
+        await $('//hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.appcompat.widget.LinearLayoutCompat/android.widget.FrameLayout/android.widget.ListView/android.widget.CheckedTextView[3]').click()
+        await $('//android.view.ViewGroup[@content-desc="button-Active"]/android.view.ViewGroup').click()
+        await $('id=android:id/button1').click()        
     });
 });
