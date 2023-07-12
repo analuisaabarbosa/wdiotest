192c4c08204ba58dfba2a6f3bf99b6fcc71d58af
ana <analuisaaugustob@gmail.com>
2023-07-12 16:27:39 -0300

    Reescrevendo testes

--- test/specs/sendingform.e2e.js
+++ test/specs/sendingform.e2e.js
@@ -4,9 +4,10 @@ describe('Testing the function FORMS in Webdriver.IO', () => {
         await $('~text-input').setValue('Texto texto')
         await $('~input-text-result').getText('Texto texto') 
         await $('~switch').click()
-        await $('//android.view.ViewGroup[@content-desc="Dropdown"]/android.view.ViewGroup/android.widget.EditText').click()
-        await $('//hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.appcompat.widget.LinearLayoutCompat/android.widget.FrameLayout/android.widget.ListView/android.widget.CheckedTextView[3]').click()
-        await $('//android.view.ViewGroup[@content-desc="button-Active"]/android.view.ViewGroup').click()
+        await $('android.widget.TextView').click()
+        await $('android.widget.CheckedTextView[3]').click()
+        await $('android.view.ViewGroup').click()
         await $('id=android:id/button1').click()        
     });
 });
+
